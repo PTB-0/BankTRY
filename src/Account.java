1bffba8418a291099ba6904cc3bf72c7415d40f5
ege <PTB@projecthebest.com>
2025-12-02 23:19:23 +0300

    daha bitmedi sadece korunmak için ekleniyor

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,8 @@
+import java.util.Random;
+import java.util.random.* ;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 public class Account{
     double bal ;
     double fee = 2;
@@ -20,12 +25,22 @@ public class Account{
         bal = bal - (vergi1+vergi2) ;
     }
 }
+/*burası yeni bir dosyaya konuşlcak ve yanına import java.utils.random.* ; eklejnecek */
 public  class investment extends Account {
     double fee = 10 ;
     public void invest(double howmny){
         if (bal < howmny + fee) {
-            System.out.println("o kadar paranız yok \n bakiyeniz : " + bal + "\n");
+            System.out.println("o kadar paranız yok \n bakiyeniz : " + bal + "\n Yatırım yapmak istediğiniz miktar :" + howmny + "ve üstüne gelen vergilendirme : " + fee +"\n yani hesaptan şu kadar çekilmeli : " + (fee + howmny));
+        }
+        else{
+            Random a = new Random();
+            a.setSeed(23124);
+            IntStream wich = a.ints();
+            DoubleStream b = a.doubles();
+            b = (double) b ;                        /*kütüphanemi yanlış acaba */
+            if (wich % 2 = 0){
+                bal += b + howmny - fee ;
+            }
         }
     }
-
 }
