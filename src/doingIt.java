5d6121c20476e25424c3cc24021e79464fe0a35c
ege <PTB@projecthebest.com>
2025-12-06 02:16:05 +0300

    güncelleme
    Signed-off-by: ege <PTB@projecthebest.com>

--- src/doingIt.java
+++ src/doingIt.java
@@ -3,7 +3,7 @@ import java.util.Scanner ;
 public class doingIt {
     public static void  main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Ne yapmak istersiniz\n SEÇENEKLER : \n 1. Hesap Oluştur \n 2. Transfer \n 3. Para ekleme \n 4. maden alma (madeni hesap) \n 5. yatırım yap (yatırım hesabı)");
+        System.out.println("Ne yapmak istersiniz\n SEÇENEKLER : \n 1. Hesap Oluştur \n 2. Transfer \n 3. Para ekleme \n 4. maden alma (madeni hesap) \n 5. yatırım yap (yatırım hesabı / madeni hesap)");
         String ask = in.nextLine() ;
         switch (ask){
             case "1." , "Hesap Oluştur" , "Hesap oluştur" , "hesap oluştur" :
